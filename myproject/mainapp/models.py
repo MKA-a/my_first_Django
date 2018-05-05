54cca65057e94f952f0dc8784f5b9f0ea0c19496
xpamou1 <mikhailxpamou@gmail.com>
2018-05-05 15:22:21 +0300

    Изменил структуру ДБ

--- myproject/mainapp/models.py
+++ myproject/mainapp/models.py
@@ -1,42 +1,24 @@
 from django.db import models
 
-
 # Create your models here.
 class Category(models.Model):
-    name = models.CharField(max_length=32, unique=True, verbose_name='Название')
-    desc = models.TextField(blank=True, verbose_name='Описание')
+    name = models.CharField(max_length=32, unique=True, verbose_name='Название Категории')
 
     def __str__(self):
         return self.name
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=32, unique=True, verbose_name='Название')
+    name = models.CharField(max_length=32, unique=True, verbose_name='Название продукта')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Название Категории')
+    short_desc = models.CharField(max_length=64, blank=True, verbose_name='Краткое описание')
+    description = models.TextField(blank=True, verbose_name='Описание')
     price = models.PositiveIntegerField(verbose_name='Цена')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    img_1 = models.ImageField(upload_to='products', null=True, default='/', verbose_name='Фотография')
+    img_2 = models.ImageField(upload_to='products', null=True, default='/', verbose_name='Фотография только перед')
+    img_3 = models.ImageField(upload_to='products', null=True, default='/', verbose_name='Фотография только зад')
+    marketing = models.IntegerField(default=0, null=True,
+                                    verbose_name='0=Без маркетинга, 1=Новинки, 2=Популярные, 3=Распродажа')
 
     def __str__(self):
-        return self.name
-
-
-class ProductImg(models.Model):
-    img = models.ImageField(upload_to='products', null=True, default='/')
-    name_product = models.ForeignKey(Product, on_delete=models.CASCADE)
-
-
-class MarketingNew(models.Model):
-    name = models.ForeignKey(Product, on_delete=models.CASCADE)
-    desc = models.TextField(blank=True)
-    img = models.ForeignKey(ProductImg, on_delete=models.CASCADE)
-
-
-class MarketingPopular(models.Model):
-    name = models.ForeignKey(Product, on_delete=models.CASCADE)
-    desc = models.TextField(blank=True)
-    img = models.ForeignKey(ProductImg, on_delete=models.CASCADE)
-
-
-class MarketingSale(models.Model):
-    name = models.ForeignKey(Product, on_delete=models.CASCADE)
-    desc = models.TextField(blank=True)
-    img = models.ForeignKey(ProductImg, on_delete=models.CASCADE)
+        return '{}-{}'.format(self.name, self.category)
